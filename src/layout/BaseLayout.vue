32bf1c0bd8ced7a0d4e1ebd124a8452de07e7dd5
weihomechen <weihome_chen@163.com>
2018-10-30 23:11:35 +0800

    feat: init

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <el-container></el-container>
+</template>
+
+<style lang="less" scoped>
+</style>
+
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="用户名">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">登陆</el-button>
+      <el-button>注册</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      form: {
+        name: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
+};
+</script>
